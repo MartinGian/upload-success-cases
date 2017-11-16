7245c2120963d1b10e8e2a7a8d09a0782095cb54
Martin Giannechini <m.giannechini@globant.com>
2017-11-16 15:28:22 -0300

    Add login Fixed bugs Working!

--- app/app.js
+++ app/app.js
@@ -1,12 +1,8 @@
 var extracter = require('../modules/extracter');
-//var transformer = require('../modules/transformer');
-var loader = require('../modules/loader');
+
+console.log("Starting..");
 
 extracter.downloadAllDocs()
     .then(extracter.downloadAllPresentations)
     .then(extracter.downloadAllPresentations)
     .then(extracter.uploadAllFiles);
-    // .then(loader.uploadAll);
-
-//.invoke(transformer.embedWaterMarks)
-//
--- modules/extracter.js
+++ modules/extracter.js
@@ -5,87 +5,154 @@ var agent = require('superagent');
 var binaryParser = require('superagent-binary-parser');
 require('superagent-proxy')(agent);
 require('q-superagent')(agent);
-var config = JSON.parse(fs.readFileSync('./config/config.json', 'utf8'));
+var config = JSON.parse(fs.readFileSync('../config/config.json', 'utf8'));
 
 var extracter = (function () {
 
   var documents;
   var presentations;
+  var files = [];
+  const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
 
   function _getDocs() {
+    console.log("Downloading Documents metadata");
+
     return agent
       .get(config.google.listAPI.replace(':folderId', config.google.folderId))
       .set({ 'Authorization': 'Bearer ' + config.google.auth.token })
       .then(function (response) {
         documents = response.body;
+        console.log(`Documents metadata downloaded: ${documents.items.length}`);
       });
   }
 
   function _getPresentations() {
+    console.log("Downloading Presentations metadata");
     return agent
       .get(config.google.presentationAPI.replace(':folderId', config.google.folderId))
       .set({ 'Authorization': 'Bearer ' + config.google.auth.token })
       .then(function (response) {
         presentations = response.body;
+        console.log(`Presentations metadata downloaded: ${presentations.items.length}`);
       });
   }
 
-  function _downloadAllFiles() {
-    var items = documents.items.concat(presentations.items);
-    var filePromises = [];
-    for (var i = 0; i < items.length; i++) {
-      var download = _getMetadata(items[i].id);
+  async function _downloadAllFiles(itemsToDownload) {
+
+    const filePromises = [];
+
+    for (var i = 0; i < itemsToDownload.length; i++) {
+      var download = _getMetadata(itemsToDownload[i].id);
       filePromises.push(download);
     }
 
-    return Q.all(filePromises).then(_uploadFiles);
+    return Q.all(filePromises).then(responses => responses.forEach(x => files.push(x.body)));
   }
 
-  function _uploadFiles(files) {
-    const documents = [];
-    const presentations = [];
+  async function _downloadManager() {
+    const items = documents.items.concat(presentations.items);
 
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i].body;
+    const max = items.length;
+    let bottomLimit = 0;
+    let topLimit = 10;
+    const step = 10;
 
-      switch (file.mimeType) {
+    console.log(`Downloading files, count: ${max}`);
+
+    while ((topLimit - max) < step) {
+
+      console.log(`Downloading files, from: ${bottomLimit} to: ${topLimit < max ? topLimit : max}`);
+
+      await _downloadAllFiles(items.slice(bottomLimit, topLimit));
+
+      console.log(`Downloaded files, from: ${bottomLimit} to: ${topLimit < max ? topLimit : max}`);
+
+      await snooze(2000);
+      bottomLimit += step;
+      topLimit += step;
+    }
+
+    console.log("Downloaded all files");
+
+    await _uploadFiles(files);
+  }
+
+  async function _uploadFiles(pFiles) {
+    console.log("Starting uploading...");
+
+    const _documents = [];
+    const _presentations = [];
+
+    for (let i = 0; i < pFiles.length; i++) {
+      const _file = pFiles[i];
+      switch (_file.mimeType) {
         case "application/vnd.google-apps.document": {
-          documents.push(file);
+          _documents.push(_file);
           break;
         }
         case "application/vnd.google-apps.presentation": {
-          presentations.push(file);
+          _presentations.push(_file);
         }
       }
     }
 
-    for (let i = 0; i < documents.length; i++) {
-      const document = documents[i];
-      const presentation = presentations.find(x => x.title == document.title);
+    console.log(`Documents: ${_documents.length}, Presentations: ${_presentations.length}`);
+    if (_documents.length !== _presentations.length)
+      console.warn(`The quantity of the Documents is not equal to Presentations`);
 
-      const data = {
-        caseName: document.title,
-        link: document.alternateLink,
-        undisclosed: false,
-        document: {
-          drive: document,
-          name: document.title + '.pdf'
-        },
-        deck: {
-          drive: presentation,
-          name: presentation.title + '.pdf'
-        }
-      };
+    var fullCases = [];
 
-      agent
-        .post(config.cms.uploadAPI)
-        .send(data)
-        .set('Cookie', config.cms.cookie)
-        .catch(function (err) {
-          console.error('Error', err);
-        })
+    for (let i = 0; i < _documents.length; i++) {
+      const _document = _documents[i];
+      const _presentation = _presentations.find(x => x.title == _document.title);
+      if (!_presentation) {
+        console.error("ERROR: could not found presentation for document: " + _document.title);
+        return;
+      }
+
+      fullCases.push({ document: _document, presentation: _presentation });
     }
+
+    for (let i = 0; i < fullCases.length; i++) {
+      try {
+
+        const _document = fullCases[i].document;
+        const _presentation = fullCases[i].presentation;
+
+        const _data = {
+          caseName: _document.title,
+          link: _document.alternateLink,
+          undisclosed: false,
+          document: {
+            drive: _document,
+            name: _document.title + '.pdf'
+          },
+          deck: {
+            drive: _presentation,
+            name: _presentation.title + '.pdf'
+          }
+        };
+
+        console.log(`[${i}] - Uploading... ${_document.title}`);
+
+        await agent
+          .post(config.cms.uploadAPI)
+          .send(_data)
+          .set('Cookie', config.cms.cookie);
+
+        console.log(`[${i}] - Uploaded... ${_document.title}`);
+
+        await snooze(5000);
+      }
+      catch (e) {
+        console.error(`[${i}] - Error on upload {document: ${_document.title}, presentation: ${_presentation.title}}`);
+        console.log(e);
+      }
+    }
+
+    console.log("Upload finished!");
   }
+
 
   function _getMetadata(fileId) {
     return agent
@@ -133,7 +200,7 @@ var extracter = (function () {
   return {
     downloadAllDocs: _getDocs,
     downloadAllPresentations: _getPresentations,
-    uploadAllFiles: _downloadAllFiles
+    uploadAllFiles: _downloadManager
   };
 
 })();
