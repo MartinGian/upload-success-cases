05a2e39a836927ca9bbf2078f3ac141bf74c6f72
Martin Giannechini <m.giannechini@globant.com>
2017-12-18 22:54:28 -0300

    Upload tags parsed with csv

--- app/app.js
+++ app/app.js
@@ -2,7 +2,9 @@ var extracter = require('../modules/extracter');
 
 console.log("Starting..");
 
-extracter.downloadAllDocs()
+extracter
+    .importTags()
+    .then(extracter.downloadAllDocs)
     .then(extracter.downloadAllPresentations)
     .then(extracter.downloadAllPresentations)
     .then(extracter.uploadAllFiles);
--- modules/extracter.js
+++ modules/extracter.js
@@ -3,15 +3,19 @@ var Q = require('q');
 var db = require('./db');
 var agent = require('superagent');
 var binaryParser = require('superagent-binary-parser');
+var parse = require('csv-parse');
+var fs = require('fs');
 require('superagent-proxy')(agent);
 require('q-superagent')(agent);
 var config = JSON.parse(fs.readFileSync('../config/config.json', 'utf8'));
 
 var extracter = (function () {
 
-  var documents;
-  var presentations;
-  var files = [];
+  let documents;
+  let presentations;
+  const files = [];
+  const tags = [];
+
   const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
 
   function _getDocs() {
@@ -47,6 +51,12 @@ var extracter = (function () {
     }
 
     return Q.all(filePromises).then(responses => responses.forEach(x => files.push(x.body)));
+  }
+
+  function _getMetadata(fileId) {
+    return agent
+      .get(config.google.fileAPI.replace(':fileId', fileId))
+      .set({ 'Authorization': 'Bearer ' + config.google.auth.token });
   }
 
   async function _downloadManager() {
@@ -104,41 +114,53 @@ var extracter = (function () {
 
     for (let i = 0; i < _documents.length; i++) {
       const _document = _documents[i];
-      const _presentation = _presentations.find(x => x.title == _document.title);
+      const _presentation = _presentations.find(x => x.title === _document.title);
+      const _tags = tags.find(x => x.caseName === _document.title);
+
       if (!_presentation) {
         console.error("ERROR: could not found presentation for document: " + _document.title);
-        return;
       }
 
-      fullCases.push({ document: _document, presentation: _presentation });
+      if (!_tags) {
+        console.error("ERROR: could not found tags for document: " + _document.title);
+      }
+
+      fullCases.push({ document: _document, presentation: _presentation, tags: _tags });
     }
 
     for (let i = 0; i < fullCases.length; i++) {
+
+      const _document = fullCases[i].document;
+      const _presentation = fullCases[i].presentation;
+      const _tags = fullCases[i].tags;
+
+      const _data = {
+        caseName: _document.title,
+        link: _document.alternateLink,
+        undisclosed: false,
+        techTags: _tags.tech,
+        businessTags: _tags.business,
+        studioTags: _tags.studio,
+        document: {
+          drive: _document,
+          name: _document.title + '.pdf'
+        },
+        deck: {
+          drive: _presentation,
+          name: _presentation.title + '.pdf'
+        }
+      };
+
       try {
-
-        const _document = fullCases[i].document;
-        const _presentation = fullCases[i].presentation;
-
-        const _data = {
-          caseName: _document.title,
-          link: _document.alternateLink,
-          undisclosed: false,
-          document: {
-            drive: _document,
-            name: _document.title + '.pdf'
-          },
-          deck: {
-            drive: _presentation,
-            name: _presentation.title + '.pdf'
-          }
-        };
 
         console.log(`[${i}] - Uploading... ${_document.title}`);
 
         await agent
           .post(config.cms.uploadAPI)
           .send(_data)
-          .set('Cookie', config.cms.cookie);
+          .set('Cookie', config.cms.cookie)
+          .proxy(config.proxy)
+          .timeout(600000);
 
         console.log(`[${i}] - Uploaded... ${_document.title}`);
 
@@ -153,51 +175,44 @@ var extracter = (function () {
     console.log("Upload finished!");
   }
 
+  function _importTags() {
+    console.log("Importing tags");
 
-  function _getMetadata(fileId) {
-    return agent
-      .get(config.google.fileAPI.replace(':fileId', fileId))
-      .set({ 'Authorization': 'Bearer ' + config.google.auth.token })
-    // .proxy(config.proxy);
-  }
+    return new Promise((resolve, reject) => {
+      try {
+        var parser = parse({ delimiter: ',' }, function (err, data) {
+          if (err) {
+            console.error("Error on tags import", e);
+            reject();
+          }
 
-  function _downloadFile(res) {
-    //console.log('_downloadFile', res.body);
-    var metadata = res.body;
-    var fileId = metadata.id;
+          // the index 0 are the titles of the columns
+          for (let i = 1; i < data.length; i++) {
+            const current = data[i];
+            const caseTags = {
+              caseName: current[0],
+              tech: current[1].split(','),
+              studio: current[2].split(','),
+              business: current[3].split(',')
+            }
 
-    return agent
-      .get(config.google.exportAPI.replace(':fileId', fileId))
-      .set({ 'Authorization': 'Bearer ' + config.google.auth.token })
-      // .proxy(config.proxy)
-      .parse(binaryParser)
-      .buffer()
-      .then(function (res) {
-        return {
-          fileBytes: res.body,
-          fileMetadata: metadata
-        }
-      });
-  }
+            tags.push(caseTags);
+          }
 
-  function _saveFile(data) {
-    var deferred = Q.defer();
-    var isDocument = (data.fileMetadata.mimeType.indexOf('doc')) > 0;
-    var suffix = isDocument ? ' - Case' : ' - Pitch';
-    var folder = isDocument ? 'docs' : 'presentations';
-    //console.log('data?: ', data);
-    //console.log('res?: ', !!res);
-    fs.writeFile(config.downloadFolder + '/' + folder + '/' + data.fileMetadata.id + suffix + '.pdf', data.fileBytes, function (err) {
-      if (err) {
-        deferred.reject();
-      } else {
-        deferred.resolve();
+          console.log("Tags import finished");
+          resolve(tags);
+        });
+
+        fs.createReadStream('../config/tags.csv').pipe(parser);
+      } catch (e) {
+        console.error("Error on tags import", e);
+        reject();
       }
     });
-    return deferred.promise;
   }
 
   return {
+    importTags: _importTags,
     downloadAllDocs: _getDocs,
     downloadAllPresentations: _getPresentations,
     uploadAllFiles: _downloadManager
